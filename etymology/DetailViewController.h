f674bf5101c4f8ac3835e56759444b3286989e94
Matt Neary <neary.matt@gmail.com>
2011-11-19 17:58:55 -0500

    Initial Commit

---
+++ etymology/DetailViewController.h
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.h
+//  etymology
+//
+//  Created by Matt on 11/19/11.
+//  Copyright 2011 St. Timothy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@class RootViewController;
+
+@interface DetailViewController : UIViewController <UIPopoverControllerDelegate, UISplitViewControllerDelegate> {
+
+}
+
+
+@property (nonatomic, retain) IBOutlet UIToolbar *toolbar;
+
+@property (nonatomic, retain) NSManagedObject *detailItem;
+
+@property (nonatomic, retain) IBOutlet UILabel *detailDescriptionLabel;
+
+@property (nonatomic, assign) IBOutlet RootViewController *rootViewController;
+
+- (IBAction)insertNewObject:(id)sender;
+
+@end
---
+++ etymology/RootViewController.h
@@ -0,0 +1,27 @@
+//
+//  RootViewController.h
+//  etymology
+//
+//  Created by Matt on 11/19/11.
+//  Copyright 2011 St. Timothy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+
+@property (nonatomic, retain) IBOutlet DetailViewController *detailViewController;
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+- (void)insertNewObject:(id)sender;
+
+@end
